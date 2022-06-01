e99c7a1c8cae725583ee716e91107a9aab416d20
kirusanov <kirusanov@gmail.com>
2022-06-02 00:42:19 +0400

    initial

---
+++ src/js/app.js
@@ -0,0 +1,40 @@
+import '../scss/app.scss'
+import '../icon.font'
+
+class App {
+    scrollToOffset = 100
+
+    constructor(config = {}) {
+        Object.keys(config).forEach((key) => {
+            if (this.hasOwnProperty(key)) {
+                this[key] = config[key]
+            }
+        })
+
+        this.initScrollTo()
+
+        // svgsprite
+        this.importAll(require.context('../svgsprite', false, /\.svg$/));
+    }
+
+    initScrollTo() {
+        document.addEventListener('click', (e) => {
+            const trigger = e.target.closest('.js-scroll[data-scroll]')
+            if (trigger) {
+                const target = document.querySelector(trigger.dataset.scroll)
+                if (target) {
+                    const y =
+                        target.getBoundingClientRect().top +
+                        window.pageYOffset -
+                        this.scrollToOffset
+                    window.scrollTo({ top: y, behavior: 'smooth' })
+                }
+            }
+        })
+    }
+
+    importAll(r) {
+        r.keys().forEach(r)
+    }
+}
+new App(appConfig || {})
