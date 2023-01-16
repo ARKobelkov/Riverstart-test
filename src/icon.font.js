1de2aca8a0207acd4da0f014224f2b1eb5080cd0
Dmitry Saveliev <d.savelyev@riverstart.ru>
2023-01-16 16:03:41 +0300

    add pug-plugin and eslint

--- src/icon.font.js
+++ src/icon.font.js
@@ -1,13 +1,13 @@
 module.exports = {
-    files: ['./icons/*.svg'],
-    fontName: 'icons',
-    classPrefix: '_',
-    baseSelector: '.icon',
-    types: ['eot', 'woff', 'woff2', 'ttf', 'svg'],
-    embed: true,
+  files: ['./icons/*.svg'],
+  fontName: 'icons',
+  classPrefix: '_',
+  baseSelector: '.icon',
+  types: ['eot', 'woff', 'woff2', 'ttf', 'svg'],
+  embed: true,
 
-    // html: true,
-    // fileName: 'app.[fontname].[hash].[ext]',
-    // dest: 'css/fonts',
-    // cssFontsUrl: 'fonts',
+  // html: true,
+  // fileName: 'app.[fontname].[hash].[ext]',
+  // dest: 'css/fonts',
+  // cssFontsUrl: 'fonts',
 }
